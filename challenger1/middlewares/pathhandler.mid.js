205ff567734464c3a970b879e2c32df1edbda0ce
German Koning <gkoning@gotoitsolutions.com>
2024-01-12 16:57:17 -0300

    Fixed bugs in general app code

--- challenger1/middlewares/pathhandler.mid.js
+++
@@ -1,8 +0,0 @@
-function pathhandler (req,res,next){
-    return res.json({
-        statuscode:404,
-        message: `${req.method} ${req.url} not found endpoint`
-    })
-}
-
-export default pathhandler
